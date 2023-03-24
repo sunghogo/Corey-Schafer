c29926ed5a97c94bd44c96db1bb7637a3d0c32bc
sunghogo <leinad5592@gmail.com>
2023-03-24 16:27:56 -0400

    L3: Integers and Floats: Lecture Example

--- intro3.py
+++ intro3.py
@@ -1,2 +1,68 @@
 # L3: Integers and FLoats - Working with Numeric Order
 # https://www.youtube.com/watch?v=khKv-8q7YmY&list=PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU&index=4&ab_channel=CoreySchafer
+
+# type() function gives data type of variable/value
+num = 3
+print(type(num))
+
+num = 3.14
+print(type(num))
+
+# Artihmetic Operators:
+# Addition:
+print(3 + 2)
+# Subtraction:
+print(3 - 2)
+# Multiplication:
+print(3 * 2)
+# Division:
+print(3 / 2)
+# Floor Division:
+print(3 // 2)
+# Exponent:
+print(3 ** 2)
+# Modulus:
+print(3 % 2)
+
+# Order of Operations with parentheses
+print(3 * 2 + 1)
+print(3 * (2 + 1))
+
+# Incrementing variables
+num = 1
+num = num + 1
+print(num)
+
+num += 1
+print(num)
+
+num *= 1
+print(num)
+
+# Built-in function
+print(abs(-3))
+print(round(3.75))
+print(round(3.75, 1))
+
+# Comparison Operators:
+# Equal:
+print(3 == 2)
+# Not Equal:
+print(3 != 2)
+# Greater Than:
+print(3 > 2)
+# Less Than:
+print(3 < 2)
+# Greater or Equal
+print(3 >= 2)
+# Less or Equal:
+print(3 <= 2)
+
+# String and number casting
+num_1 = '100'
+num_2 = '200'
+print(num_1 + num_2)
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+print(num_1 + num_2)
