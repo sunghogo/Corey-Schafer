9c9f5383ad47acd083da3b2b01842b369d92c484
sunghogo <leinad5592@gmail.com>
2023-07-21 12:50:05 -0400

    Python Tutorial for Beginners 1: Install and Setup for Mac and Windows

---
+++ 1 - Install and Setup for Mac and Windows.py
@@ -0,0 +1,5 @@
+# Python Tutorial for Beginners 1: Install and Setup for Mac and Windows
+# https://www.youtube.com/watch?v=YYXdXT2l-Gg&list=PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU&ab_channel=CoreySchafer
+
+# Print Welcome Message
+print('Hello World')
